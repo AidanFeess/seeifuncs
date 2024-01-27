67332a586b1d1ccba434eacbe309523e1eca23e4
Aidan Feess <145503050+AidanFeess@users.noreply.github.com>
2024-01-26 22:30:59 -0600

    add match

--- funcs.lua
+++ funcs.lua
@@ -1,1 +1,11 @@
+local libraries = {
+	simplecode = {},
+}
 
+function libraries.simplecode.match(actual : variable, cases : "list of cases") : "Matches a variable to a case and executes that code"
+	for var, case in pairs(cases) do
+		if var == actual then case() end
+	end
+end
+
+return libraries
